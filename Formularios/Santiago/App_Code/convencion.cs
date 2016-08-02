771a00c89910ea949ce8d86ee267df9c156ca941
SLoaiza <sloaiza44@misena.edu.co>
2016-08-02 12:48:42 -0500

    Formularios (Convencion, inasistencia, institución)

---
+++ Formularios/Santiago/App_Code/convencion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+/// <summary>
+/// Descripción breve de convencion
+/// </summary>
+public class convencion
+{
+
+    public string guardar_convencion(String conven_nombre)
+    {
+        string rdo = "1";
+        try
+        {
+            var conex = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion_asp_con_clave"].ConnectionString);
+            var insertar = "insert into convencion values('" + conven_nombre + "')";
+            var comando = new SqlCommand(insertar, conex);
+            conex.Open();
+            int resultado = comando.ExecuteNonQuery();
+            if (resultado == 0)
+            {
+                rdo = "0";
+                conex.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            rdo = e.Message;
+        }
+        return rdo;
+    }
+    public convencion()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
